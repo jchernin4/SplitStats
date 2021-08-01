ab8fcc7553d09f689d9506a0f29f04056eb96447
jchernin4 <jeremychernin@gmail.com>
2021-08-01 10:45:06 -0400

    Logback + more setting up

---
+++ src/main/java/SplitStats/BotContainer.java
@@ -0,0 +1,7 @@
+package SplitStats;
+
+import net.dv8tion.jda.api.JDA;
+
+public class BotContainer {
+    public static JDA jda;
+}
---
+++ src/main/java/SplitStats/Settings.java
@@ -0,0 +1,6 @@
+package SplitStats;
+
+public class Settings {
+    public static final String TOKEN = "ODcxNDAyMjYxNDIxNjQ5OTcw.YQaymA.CQCbMxPvsAH-8JplfTonq2g2o1E";
+    public static final String INVITE_LINK = "https://discord.com/api/oauth2/authorize?client_id=871402261421649970&permissions=240585657408&scope=bot";
+}
